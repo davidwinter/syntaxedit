df33d654fa265e4b3342b274f9425105ba0730d9
David Winter <i@djw.me>
2023-01-22 10:49:45 +0000

    refactor: highlightslot test using pytest-mock

--- tests/test_highlightslot.py
+++ tests/test_highlightslot.py
@@ -5,40 +5,6 @@ from pygments.lexers import get_lexer_by_name
 from syntaxedit.highlightslot import HighlightSlot
 
 
-class WidgetMock:
-    def __init__(self, text=""):
-        self.text = text
-        self.html = ""
-        self._cursorPosition = 5
-
-    def cursorPosition(self):
-        return self._cursorPosition
-
-    def setCursorPosition(self, position):
-        self._cursorPosition = position
-
-    def syntax(self):
-        return "Markdown"
-
-    def theme(self):
-        return "solarized-light"
-
-    def editorFont(self):
-        return "Courier New"
-
-    def toPlainText(self):
-        return self.text
-
-    def toHtml(self):
-        return self.html
-
-    def setHtml(self, html):
-        self.html = html
-
-    def blockSignals(self, value):
-        pass
-
-
 def highlight_text(text):
     return highlight(
         text,
@@ -53,10 +19,14 @@ def highlight_text(text):
     )
 
 
-def test_text_highlighted():
-    widget = WidgetMock("# Hello\n")
-    slot = HighlightSlot(widget)
+def test_text_highlighted(mocker):
+    widget = mocker.MagicMock()
+    widget.syntax.return_value = "Markdown"
+    widget.theme.return_value = "solarized-light"
+    widget.editorFont.return_value = "Courier New"
+    widget.toPlainText.return_value = "# Hello\n"
 
+    slot = HighlightSlot(widget)
     slot.execute()
 
-    assert widget.toHtml() == highlight_text("# Hello\n")
+    widget.setHtml.assert_called_once_with(highlight_text("# Hello\n"))
